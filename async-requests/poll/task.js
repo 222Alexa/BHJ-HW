651addfb20057db06de303497370ca37e20ecd50
Alexandra Titova <auto-steklo@mail.ru>
2021-08-17 02:25:10 +0300

    seven2

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,145 +1,54 @@
 'use strict';
 
-
-/*const title = document.querySelector('#poll__title');
+const poll = document.querySelector('#poll__title');
 const answers = document.querySelector('#poll__answers');
-
 
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php', true);
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
 xhr.send();
-
 xhr.addEventListener('readystatechange', () => {
-	if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-		const response = JSON.parse(xhr.responseText);
-		title.textContent = response.data.title;
-		for (let key in response.data.answers) {
+	if (xhr.readyState === xhr.DONE && xhr.status == 200) {
+
+		const ansXhr = JSON.parse(xhr.responseText);
+
+		poll.textContent = ansXhr.data.title;
+
+		for (let key in ansXhr.data.answers) {
 			answers.insertAdjacentHTML('beforeend', ` <button class="poll__answer">
-              ${response.data.answers[key]}
+              ${ansXhr.data.answers[key]}
             </button>`);
-			console.log(response.data.answers[key]);
+			
 		}
-	};
-});
+		//alert('Спасибо, Ваш голос засчитан!') 
+		for (let answer in ansXhr.data.answers) {
 
-document.getElementById('poll__answers').addEventListener('click', (event) => {
-	if (event.target.classList.contains('poll__answer')) {
-		alert('спасибо, Ваш голос засчитан!');
 
-		const xhr = new XMLHttpRequest();
-		xhr.open("POST", "https://netology-slow-rest.herokuapp.com/poll.php");
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.send(`vote=${title.dataset.id}&answer=${event.target.id}`);
+			document.addEventListener('click', () => {
 
-		xhr.onreadystatechange = function () {//этот код правильно считает, но не меняет заголовок
-			if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-				answers.innerHTML = "";
-				const response = JSON.parse(xhr.responseText);
-				const pollAnswers = response.stat;
-				let votesAll = 0;
-				pollAnswers.forEach(answer => {
-					votesAll += answer.votes;
+				// отображение результатов
+				const xhr = new XMLHttpRequest();
+				xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
+				xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+				xhr.send(`vote=${ansXhr.id}&answer=${answer}`)
+
+				xhr.addEventListener('readystatechange', () => {
+					if (xhr.readyState === xhr.DONE && xhr.status == 200) {
+						const resXhr = JSON.parse(xhr.responseText);
+						
+						const votesAll = Object.values(resXhr.stat).reduce((sum, item) => sum + item.votes, 0);
+
+						answers.innerHTML = '';
+						// убираем кнопки голосования
+						for (let result in resXhr.stat) {
+							answers.innerHTML += `<div>${resXhr.stat[result].answer}: <b>${(resXhr.stat[result].votes / votesAll * 100).toFixed(2)}%</b></div>`;
+						}
+					}
+
 				});
-				pollAnswers.forEach(answer => {
-					answers.insertAdjacentHTML("beforeEnd",
-						`<div class="poll__answer">
-                  ${answer.answer}: <b>${(100 * answer.votes / votesAll).toFixed(2)}%</b>
-                </div>
-                `)
-				});
-				
-				xhr.send(`vote=${response.id}&answer=${target.textContent}`);
 
-			}
-
-		}
-		
-	}
-})
-
-
-
-
-
-
-*/
-
-
-
-//и всегда включается пауза дебаггера и  WebSocket connection to 'ws://localhost:8125/' failed: Error in connection establishment: net::ERR_CONNECTION_REFUSED
-//createWebSocket @ VM1429:1047
-//RemoteFunctions @ VM1429:1062
-//(anonymous) @ VM1429:1078
-//(anonymous) @ task.js:72  - что это?
-
-
-const title = document.querySelector('#poll__title');
-const answers = document.querySelector('#poll__answers');
-
-async function markUp() {
-	const response = await request();
-
-
-	title.textContent = response.data.title;
-	for (let key in response.data.answers) {
-		answers.insertAdjacentHTML('beforeend', ` <button class="poll__answer">
-              ${response.data.answers[key]}
-            </button>`);
-	}
-}
-
-markUp();
-
-async function request() {
-	let response = await fetch('https://netology-slow-rest.herokuapp.com/poll.php');
-	let obj = await response.json();
-
-	return obj;
-}
-
-
-
-
-function getStatistics(event) {
-	let statistic = '';
-	let sumOfVotes = 0;
-	fetch('https://netology-slow-rest.herokuapp.com/poll.php', {
-			method: 'POST',
-			headers: {
-				'Content-type': 'application/x-www-form-urlencoded',
-			},
-			body: `vote=${title.dataset.id}&answer=${event.target.id}`,
-		})
-		.then((response) => response.json())
-		.then((obj) => {
-
-			console.log(obj.stat)
-			obj.stat.forEach(elem => {//этот код  меняет заголовок, но считает неправильно и везде вставляет животных
-				sumOfVotes += elem.votes;
-				console.log(sumOfVotes);
-				statistic += `
-						<div class ="poll__answer">
-						${elem.answer}:<strong>${(elem.votes * 100/sumOfVotes)// ошибка здесь, но я ее не вижу
-						.toFixed(2)}%</strong>
-						</div>`;
-
-				
-				
 			});
-		answers.innerHTML = statistic;
-		})
-		.catch((err) => console.log(err));
-}
 
-
-
-document.addEventListener('click', (event) => {
-	if (!event.target.classList.contains('poll__answer')) {
-		return;
+		}
 	}
-	getStatistics(event);
-	alert('спасибо, Ваш голос засчитан!');
 });
-
-
 
--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,17 +1,55 @@
 'use strict ';
 
-/*let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com', true);
-xhr.send();
-xhr.addEventListener('readystatechange', () => {
-	if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-		document.getElementById('loader').classList.remove('loader_active');
-	};
-	 const coin = JSON.parse(xhr.responseText);
-	document.getElementById('items').innerHTML = '';
-	getAnswer(coin.response.Valute);
-	localStorage.setItem('currency', xhr.responseText);
+window.addEventListener('load', () => {
+
+	getLoaderStorage();
+	request()
+
 });
+
+async function request() {
+
+	try {
+		const fetchResp = await fetch('https://netology-slow-rest.herokuapp.com');
+
+		if (!fetchResp.ok) {
+			throw new Error(`Ошибка по адресу ${url}, статус ошибки ${fetchResp.status}`);
+		}
+		const obj = await fetchResp.json();
+		const lastCurrency = obj.response.Valute;
+		
+		document.querySelector('#items').innerHTML = '';
+		getAnswer(lastCurrency);
+		addCurrencyInStorage(lastCurrency);
+		
+	}
+
+	catch (err) {
+
+		console.log(err)
+	}
+};
+
+	/*
+	function request() {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://netology-slow-rest.herokuapp.com', true);
+	xhr.send();
+	xhr.addEventListener('readystatechange', () => {
+		if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+			const data = JSON.parse(xhr.responseText);
+			const lastCurrency = data.response.Valute;
+			document.querySelector('#items').innerHTML = '';
+			getAnswer(lastCurrency);
+			addCurrencyInStorage(lastCurrency);
+			document.getElementById('loader').classList.remove('loader_active');
+
+		};
+	});
+}*/
+
+
+
 
 function getAnswer(item) {
 	for (let key in item) {
@@ -29,73 +67,47 @@ function getAnswer(item) {
       </div>
       `);
 	}
+	
+	
 }
 
-window.addEventListener('load', () => {
+function addCurrencyInStorage(item) {
 
-	if (localStorage.getItem("currency")) {
-		const lastCurrency = JSON.parse(localStorage.getItem('currency'));
-		document.getElementById('loader').classList.remove('loader_active');
-		getAnswer(lastCurrency);
-	}
-});
-*/
-window.addEventListener('load', () => {
-//сюда данные из ЛС
-request();
-});
+	let lastCurrency = getStorage();
+	lastCurrency.push(item);
 
-function request() {
-
-	fetch('https://netology-slow-rest.herokuapp.com')
-		.then(response => response.json())
-		.then(obj => {
-			template(obj.response.Valute);
-			loader.classList.remove("loader_active");
-		})
-		.catch((error) => console.log('OOOops!'));
+	setStorage(lastCurrency);
+	console.log(lastCurrency)
 
 }
 
-
-
-function template(item) {
-	for (let key in item) {
-		document.getElementById('items').insertAdjacentHTML('beforeend', `
-      <div class="item">
-      <div class="item__code">
-      ${item[key].CharCode}
-      </div>
-      <div class="item__value">
-      ${item[key].Value}
-      </div>
-      <div class="item__currency">
-      руб.
-      </div>
-      </div>
-      `);
-
-	}
-}
-
-
-
-function loadedCurrency(){
-
-}
-
-function setStorage(itemsTask) {
-
-	return localStorage.setItem('items', JSON.stringify(currency));
-}
 
 function getStorage() {
-	let currencyList = localStorage.getItem('currency');
-	currencyList = currencyList ? JSON.parse(currencyList) : [];
 
-	return currencyList;
+	const lastCurrency = localStorage.getItem('lastCurrency');
+	return lastCurrency ? JSON.parse(lastCurrency) : [];
+
 }
 
+function setStorage(item) {
+	return localStorage.setItem('lastCurrency', JSON.stringify(item));
+}
+
+function getLoaderStorage() {
+
+	const lastCurrency = getStorage();
+
+	if (!lastCurrency) {
+		return;
+	}
+	else {
+		document.getElementById('loader').classList.remove('loader_active');
+		let { [Object.keys(lastCurrency).pop()]: lastItem } = lastCurrency;
+		getAnswer(lastItem);
+		
+	}
+
+}
 
 
 
